395f8ab5ea78ae57adb6a59eb3a80a3b439d2f95
Ankit Kumar <ankitk.developer@gmail.com>
2025-03-26 18:31:54 +0530

    Design changes for getstarted

--- components/home/GetStarted.tsx
+++ components/home/GetStarted.tsx
@@ -7,13 +7,13 @@ const GetStarted = () => {
     <div className="">
       <div
         style={{ clipPath: "polygon(0 1%, 100% 0, 100% 100%, 0 90%)" }}
-        className="w-screen h-[70vh] bg-[#d9f7f4]"
+        className="w-screen h-[65vh] bg-[#d9f7f4]"
       ></div>
       <div className="absolute w-full -top-[20%]">
         <ConcentricCircles />
       </div>
       <div>
-        <Image src="/images/get-started/man.png" alt="hero" width={500} height={500} className="absolute top-[15%]" />
+        <Image src="/images/get-started/man.png" alt="hero" width={500} height={500} className="absolute top-[11%]" />
       </div>
       <div>
         <Image
@@ -21,7 +21,7 @@ const GetStarted = () => {
           alt="hero"
           width={80}
           height={80}
-          className="absolute top-[22%] right-[11%]"
+          className="absolute top-[17%] right-[11%]"
         />
       </div>
       <div>
@@ -30,7 +30,7 @@ const GetStarted = () => {
           alt="hero"
           width={70}
           height={70}
-          className="absolute top-[17%] left-[12%]"
+          className="absolute top-[13%] left-[12%]"
         />
       </div>
       {/* <GetStartedCarousel /> */}
--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -8,13 +8,13 @@ const Home = () => {
   return (
     <div className="font-nunito">
       <GetStarted />
-      <div className=" text-4xl font-extrabold text-main text-center">
+      <div className=" text-4xl mt-2 font-extrabold text-main text-center">
         Spend Smarter <br /> Save More
       </div>
       <div className="absolute bottom-8 w-full">
         <div className="flex justify-center">
           <button
-            className="w-[80%] border-[1px] font-semibold border-black rounded-full px-4 py-4 text-2xl shadow-2xl text-white"
+            className="w-[80%] border-[1px] font-semibold border-black rounded-full px-4 py-[14px] text-2xl shadow-2xl text-white"
             style={{
               background: "linear-gradient(145deg, #799c9c, #678c8c)",
             }}
@@ -22,7 +22,7 @@ const Home = () => {
             Get Started
           </button>
         </div>
-        <div className="flex justify-center mt-1">
+        <div className="flex justify-center mt-2">
           Already an existing user ? <span className="ml-1"> Login</span>
         </div>
       </div>
